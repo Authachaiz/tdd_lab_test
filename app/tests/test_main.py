1a5ddc26cc2cb93326f31f8329888ad61ad72c02
Authachai <115388830+Authachaiz@users.noreply.github.com>
2023-03-22 14:02:53 +0700

    Update test_main.py

--- app/tests/test_main.py
+++ app/tests/test_main.py
@@ -4,24 +4,18 @@ from app.main import app
 
 client = TestClient(app)
 
-
-# def test_read_main():
-#     response = client.get("/")
-#     assert response.status_code == 200
-#     assert response.json() == {"Hello": "World1"}
-
 def test_read_main():
     response = client.get("/")
     assert response.status_code == 200
     assert response.json() == {"Hello": "World"}
 
-def test_callname():
+def test_callname_get():
     name = "Authachai"
     response = client.get(f"/callname/{name}")
     assert response.status_code == 200
     assert response.json() == {"hello": name}
 
-def test_callname():
+def test_callname_post():
     name = "Iffan"
     response = client.post("/callname", json={"name": name})
     assert response.status_code == 200
